1e5e3da55ca75133249674588d6e406294c8435d
Hussain Saad <hs777it@gmail.com>
2019-06-06 01:13:44 +0300

    End Of Section 7

---
+++ ZwajApp-SPA/src/app/_guards/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authservice:AuthService,private router:Router,private alertify:AlertifyService) {}
+  canActivate(): boolean {
+    if(this.authservice.loggedIn())
+    {
+      return true;
+    }
+    this.alertify.error('يجب تسجيل الدخول أولا');
+    this.router.navigate(['']);
+    return false;
+  }
+}
